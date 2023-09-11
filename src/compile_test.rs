0cfdb0b67c53a17e20403d5bf6abe595ff2d2666
Michał Kostrubiec <fractalfirdev@gmail.com>
2023-09-11 17:57:09 +0200

    Some work towards using a custom target triple.

--- src/compile_test.rs
+++ src/compile_test.rs
@@ -1,3 +1,34 @@
+#[cfg(any(target_os = "linux",target_os = "macos"))]
+fn test_dotnet_executable(exec_path:&str,test_dir:&str){
+    // Execute the test assembly
+    let out = std::process::Command::new("mono")
+    .current_dir(test_dir)
+    .args([exec_path])
+    .output()
+    .expect("failed to run test assebmly!");
+    let stderr = String::from_utf8(out.stderr).expect("Stdout is not UTF8 String!");
+    if !stderr.is_empty() {
+        panic!("Test program failed with message {stderr:}");
+    }
+}
+//TODO: While we can ensure all exec_path's come from the test runner, it is also very important to ensure this:
+//1. Always executes test
+//2. cannot be used to run any arbitray executable not produced by the compiler backend. This is not an issue when using mono, since all of our executables must be .NET assemblies to be executed.
+// This is most likely not an issue at all, I just prefer being unecesarly paranoid over shipping broken code.
+// Idealy, we would prefer a sanboxed enviroment over this for all targets, but setting it up may require some more effort.
+#[cfg(target_os = "windows")]
+fn test_dotnet_executable(exec_path:&str,test_dir:&str){
+    todo!("Executing test assemblies on windows is not yet supported, since I am not sure if this is the right way to go about it. Comment out this line if you want to renable this kind of tests.");
+    // Execute the test assembly
+    let out = std::process::Command::new(exec_path)
+    .current_dir(test_dir)
+    .output()
+    .expect("failed to run test assebmly!");
+    let stderr = String::from_utf8(out.stderr).expect("Stdout is not UTF8 String!");
+    if !stderr.is_empty() {
+        panic!("Test program failed with message {stderr:}");
+    }
+}
 macro_rules! test_lib {
     ($test_name:ident) => {
         #[test]
@@ -12,6 +43,7 @@ macro_rules! test_lib {
             // Compiles the test project
             let out = std::process::Command::new("rustc")
                 .current_dir("./test/out")
+                //.env("RUST_TARGET_PATH","../../")
                 .args([
                     "-O",
                     "--crate-type=lib",
@@ -20,6 +52,8 @@ macro_rules! test_lib {
                     concat!("../", stringify!($test_name), ".rs"),
                     "-o",
                     concat!("./", stringify!($test_name), ".dll"),
+                    //"--target",
+                   // "clr64-unknown-clr"
                 ])
                 .output()
                 .expect("failed to execute process");
@@ -48,6 +82,7 @@ macro_rules! run_test {
                 .expect("could not build the backend");
             // Compiles the test project
             let out = std::process::Command::new("rustc")
+                //.env("RUST_TARGET_PATH","../../")
                 .current_dir(test_dir)
                 .args([
                     "-O",
@@ -56,6 +91,8 @@ macro_rules! run_test {
                     concat!("./", stringify!($test_name), ".rs"),
                     "-o",
                     concat!("./", stringify!($test_name), ".exe"),
+                    //"--target",
+                    //"clr64-unknown-clr"
                 ])
                 .output()
                 .expect("failed to execute process");
@@ -67,17 +104,8 @@ macro_rules! run_test {
                     .expect("rustc error contained non-UTF8 characters.");
                 panic!("stdout:\n{stdout}\nstderr:\n{stderr}");
             }
-            // Execute the test assembly
             let exec_path = concat!("../", stringify!($test_name), ".exe");
-            let out = std::process::Command::new("mono")
-                .current_dir(test_dir)
-                .args([exec_path])
-                .output()
-                .expect("failed to run test assebmly!");
-            let stderr = String::from_utf8(out.stderr).expect("Stdout is not UTF8 String!");
-            if !stderr.is_empty() {
-                panic!("Test program failed with message {stderr:}");
-            }
+            test_dotnet_executable(exec_path,test_dir);
         }
     };
 }
