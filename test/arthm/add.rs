27fc6bd7e501b37508222e72d8c953dcfba31d62
Michał Kostrubiec <fractalfirdev@gmail.com>
2023-09-11 17:57:25 +0200

    Some work towards using a custom target triple.

---
+++ test/arthm/add.rs
@@ -0,0 +1,29 @@
+#![feature(lang_items)]
+#![allow(internal_features)]
+#![no_std]
+#![feature(start)]
+#![feature(core_intrinsics)]
+include!("../common.rs");
+fn main(){
+    // Test addition of different types
+    test_eq!(black_box(2_i8) + 2, 4);
+    test_eq!(black_box(2_u8) + 2, 4);
+    test_eq!(black_box(2_i16) + 2, 4);
+    test_eq!(black_box(2_u16) + 2, 4);
+    test_eq!(black_box(2_i32)+ 2, 4);
+    test_eq!(black_box(2_u32) + 2, 4);
+    test_eq!(black_box(2_i64)+ 2, 4);
+    test_eq!(black_box(2_u64) + 2, 4);
+    test_eq!(black_box(2.0_f32) + 2.0, 4.0);
+    test_eq!(black_box(2.0_f64) + 2.0, 4.0);
+    // Test addition of negative values
+    test_eq!(black_box(2_i8) + black_box(-2), 0);
+    test_eq!(black_box(2_i16) + black_box(-2), 0);
+    test_eq!(black_box(2_i32) + black_box(-2), 0);
+    test_eq!(black_box(2_i64) + black_box(-2), 0);
+    test_eq!(black_box(2_f32) + black_box(-2.0), 0.0);
+    test_eq!(black_box(2_f64) + black_box(-2.0), 0.0);
+    // Test overflows 
+    test_eq!(black_box(0xFF_u8).wrapping_add(1), 0);
+    test_eq!(black_box(127_i8).wrapping_add(1), -128);
+}
---
+++ test/common.rs
@@ -0,0 +1,44 @@
+#[lang = "eh_personality"]
+fn rust_eh_personality() {}
+use core::panic::PanicInfo;
+extern "C"{
+    fn puts(msg:*const u8);
+} 
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    core::intrinsics::abort();
+}
+#[start]
+fn start(_argc:isize,_argv: *const *const u8) -> isize{
+    main();
+    // 'All OK!' message
+    let msg = 0x416C6C204F4B2100_i64;
+    unsafe{puts(core::ptr::addr_of!(msg).cast())}
+    0
+}
+use core::intrinsics::black_box;
+#[allow(unused_macros)]
+macro_rules! assert{
+    ($condition:expr)=>{
+        if !black_box($condition){
+            core::intrinsics::abort();
+        }
+    }
+}
+#[allow(unused_macros)]
+macro_rules! test_eq{
+    ($a:expr,$b:expr)=>{
+        if black_box($a) != black_box($b){
+            core::intrinsics::abort();
+        }
+    }
+}
+#[allow(unused_macros)]
+macro_rules! assert_ne{
+    ($a:expr,$b:expr)=>{
+        if black_box($a) == black_box($b){
+            core::intrinsics::abort();
+        }
+    }
+}
+
