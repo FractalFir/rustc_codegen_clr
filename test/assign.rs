37c1b3e08e4ec32954d7402b07a2e3baa18eb296
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-10-02 18:40:03 +0000

    Fixed test to assign from behind ref
    This ensures that a load instruction is emitted in addition to a store
    instruction.

--- test/assign.rs
+++ test/assign.rs
@@ -1,59 +1,59 @@
 #[no_mangle]
-pub extern fn assign_i8(place: &mut i8, value: i8) {
-    *place = value;
+pub extern fn assign_i8(place: &mut i8, value: &i8) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_i16(place: &mut i16, value: i16) {
-    *place = value;
+pub extern fn assign_i16(place: &mut i16, value: &i16) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_i32(place: &mut i32, value: i32) {
-    *place = value;
+pub extern fn assign_i32(place: &mut i32, value: &i32) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_i64(place: &mut i64, value: i64) {
-    *place = value;
+pub extern fn assign_i64(place: &mut i64, value: &i64) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_isize(place: &mut isize, value: isize) {
-    *place = value;
+pub extern fn assign_isize(place: &mut isize, value: &isize) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_u8(place: &mut u8, value: u8) {
-    *place = value;
+pub extern fn assign_u8(place: &mut u8, value: &u8) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_u16(place: &mut u16, value: u16) {
-    *place = value;
+pub extern fn assign_u16(place: &mut u16, value: &u16) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_u32(place: &mut u32, value: u32) {
-    *place = value;
+pub extern fn assign_u32(place: &mut u32, value: &u32) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_u64(place: &mut u64, value: u64) {
-    *place = value;
+pub extern fn assign_u64(place: &mut u64, value: &u64) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_usize(place: &mut usize, value: usize) {
-    *place = value;
+pub extern fn assign_usize(place: &mut usize, value: &usize) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_f32(place: &mut f32, value: f32) {
-    *place = value;
+pub extern fn assign_f32(place: &mut f32, value: &f32) {
+    *place = *value;
 }
 
 #[no_mangle]
-pub extern fn assign_f64(place: &mut f64, value: f64) {
-    *place = value;
+pub extern fn assign_f64(place: &mut f64, value: &f64) {
+    *place = *value;
 }
