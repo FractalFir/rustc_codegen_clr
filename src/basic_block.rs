3f515bfe57e858a8b4d37c50d2ae713fc50bb727
Michał Kostrubiec <fractalfirdev@gmail.com>
2023-10-22 22:28:28 +0200

    Fixed missing files

---
+++ src/basic_block.rs
@@ -0,0 +1,89 @@
+use crate::cil_op::CILOp;
+use rustc_middle::mir::UnwindAction;
+#[derive(Debug)]
+struct BasicBlock {
+    unwind_action: UnwindAction,
+    ops: Vec<CILOp>,
+}
+impl BasicBlock {
+    pub fn new(ops: Vec<CILOp>, unwind_action: UnwindAction) -> Self {
+        Self { unwind_action, ops }
+    }
+    pub fn may_merge(&self, other: &Self) -> bool {
+        match (self.unwind_action, other.unwind_action) {
+            (UnwindAction::Continue, UnwindAction::Continue) => true,
+            (UnwindAction::Continue, UnwindAction::Unreachable)
+            | (UnwindAction::Unreachable, UnwindAction::Continue) => true,
+            (UnwindAction::Unreachable, UnwindAction::Unreachable) => true,
+            (UnwindAction::Cleanup(a), UnwindAction::Cleanup(b)) => a == b,
+            (UnwindAction::Terminate(_), UnwindAction::Terminate(_)) => true,
+            (_, _) => false,
+        }
+    }
+    pub fn into_ops(bbs: &[Self]) -> Vec<CILOp> {
+        todo!("Can't convert basic blocks {bbs:?} into ops yet!");
+    }
+}
+#[test]
+fn exceptions() {
+    use crate::{cil_op::CallSite, function_sig::FnSig, r#type::Type};
+    let bb0 = BasicBlock::new(
+        vec![
+            CILOp::LDArg(0),
+            CILOp::LDArg(1),
+            CILOp::Add,
+            CILOp::Dup,
+            CILOp::STLoc(0),
+            CILOp::Call(CallSite::boxed(
+                None,
+                "malloc".into(),
+                FnSig::new(&[Type::ISize], &Type::Ptr(Type::Void.into())),
+                true,
+            )),
+            CILOp::GoTo(1),
+        ],
+        UnwindAction::Continue,
+    );
+    let bb1 = BasicBlock::new(
+        vec![
+            CILOp::STLoc(1),
+            CILOp::LDLoc(1),
+            CILOp::Call(CallSite::boxed(
+                None,
+                "do_sth_with_a_buff".into(),
+                FnSig::new(&[Type::Ptr(Type::Void.into())], &Type::Void),
+                true,
+            )),
+            CILOp::GoTo(2),
+        ],
+        UnwindAction::Cleanup(3_u32.into()),
+    );
+    let bb2 = BasicBlock::new(
+        vec![
+            CILOp::LDLoc(1),
+            CILOp::Call(CallSite::boxed(
+                None,
+                "free".into(),
+                FnSig::new(&[Type::Ptr(Type::Void.into())], &Type::Void),
+                true,
+            )),
+            CILOp::Ret,
+        ],
+        UnwindAction::Continue,
+    );
+    let bb3 = BasicBlock::new(
+        vec![
+            CILOp::LDLoc(1),
+            CILOp::Call(CallSite::boxed(
+                None,
+                "free".into(),
+                FnSig::new(&[Type::Ptr(Type::Void.into())], &Type::Void),
+                true,
+            )),
+            CILOp::Rethrow,
+        ],
+        UnwindAction::Continue,
+    );
+    let bbs = vec![bb0, bb1, bb2, bb3];
+    let ops = BasicBlock::into_ops(&bbs);
+}
---
+++ src/checked_binop.rs
@@ -0,0 +1,75 @@
+use rustc_middle::mir::{BinOp, Operand};
+use rustc_middle::ty::{Instance, IntTy, Ty, TyCtxt, TyKind, UintTy};
+
+use crate::cil_op::CILOp;
+use crate::r#type::Type;
+/// Preforms an checked binary operation.
+pub(crate) fn binop_checked<'tcx>(
+    binop: BinOp,
+    operand_a: &Operand<'tcx>,
+    operand_b: &Operand<'tcx>,
+    tcx: TyCtxt<'tcx>,
+    method: &rustc_middle::mir::Body<'tcx>,
+    method_instance: Instance<'tcx>,
+) -> Vec<CILOp> {
+    let ops_a = crate::operand::handle_operand(operand_a, tcx, method, method_instance);
+    let ops_b = crate::operand::handle_operand(operand_b, tcx, method, method_instance);
+    let ty_a = operand_a.ty(&method.local_decls, tcx);
+    let ty_b = operand_b.ty(&method.local_decls, tcx);
+    assert_eq!(ty_a, ty_b);
+    let ty = Type::from_ty(ty_a, tcx);
+    todo!("Checked ops should return a tuple!");
+    match binop {
+        BinOp::Mul | BinOp::MulUnchecked => [ops_a, ops_b, mul(ty).into()]
+            .into_iter()
+            .flatten()
+            .collect(),
+        BinOp::Add => [ops_a, ops_b, add(ty).into()]
+            .into_iter()
+            .flatten()
+            .collect(),
+        BinOp::Sub => [ops_a, ops_b, sub(ty).into()]
+            .into_iter()
+            .flatten()
+            .collect(),
+        _ => todo!("Can't preform checked op {binop:?}"),
+    }
+}
+fn mul(tpe: Type) -> &'static [CILOp] {
+    match tpe {
+        Type::I16 => &[CILOp::MulOvf, CILOp::ConvI16(true)],
+        Type::I32 => &[CILOp::MulOvf],
+        Type::I64 => &[CILOp::MulOvf],
+        _ => todo!("Can't preform checked mul on type {tpe:?} yet!"),
+    }
+}
+fn add(tpe: Type) -> &'static [CILOp] {
+    match tpe {
+        Type::I8 => &[CILOp::AddOvf, CILOp::ConvI8(true)],
+        Type::U8 => &[CILOp::AddOvfUn, CILOp::ConvU8(true)],
+        Type::I16 => &[CILOp::AddOvf, CILOp::ConvI16(true)],
+        Type::U16 => &[CILOp::AddOvfUn, CILOp::ConvU16(true)],
+        Type::I32 => &[CILOp::AddOvf],
+        Type::U32 => &[CILOp::AddOvfUn],
+        Type::I64 => &[CILOp::AddOvf],
+        Type::U64 => &[CILOp::AddOvfUn],
+        Type::USize => &[CILOp::AddOvfUn],
+        Type::ISize => &[CILOp::AddOvf],
+        _ => todo!("Can't preform checked add on type {tpe:?} yet!"),
+    }
+}
+fn sub(tpe: Type) -> &'static [CILOp] {
+    match tpe {
+        Type::I8 => &[CILOp::SubOvf, CILOp::ConvI8(true)],
+        Type::U8 => &[CILOp::SubOvfUn, CILOp::ConvU8(true)],
+        Type::I16 => &[CILOp::SubOvf, CILOp::ConvI16(true)],
+        Type::U16 => &[CILOp::SubOvfUn, CILOp::ConvU16(true)],
+        Type::I32 => &[CILOp::SubOvf],
+        Type::U32 => &[CILOp::SubOvfUn],
+        Type::I64 => &[CILOp::SubOvf],
+        Type::U64 => &[CILOp::SubOvfUn],
+        Type::USize => &[CILOp::SubOvfUn],
+        Type::ISize => &[CILOp::SubOvf],
+        _ => todo!("Can't preform checked add on type {tpe:?} yet!"),
+    }
+}
