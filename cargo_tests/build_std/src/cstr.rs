f279d52b72ce942abe7782a5f11075b59091bd60
Michał Kostrubiec <fractalfirdev@gmail.com>
2024-04-23 20:57:46 +0200

    Added a missing test file

---
+++ cargo_tests/build_std/src/cstr.rs
@@ -0,0 +1,9 @@
+use crate::*;
+use std::ffi::CStr;
+pub fn test_cstr(){
+    unsafe {
+        // This test used to fail becasue the call to libc strlen always returned 0. This happened because preservesig was not set, and the return value was replaced by a HRESULT.
+        let slice = CStr::from_ptr("Testing cstr!\n\0".as_ptr() as *const i8);
+        printf(slice.as_ptr() as *const i8);
+    }
+}
